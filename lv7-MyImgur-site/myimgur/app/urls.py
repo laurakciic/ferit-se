0870538ec330fb9f1d84854eddedc8a5a32e6e47
Laura Kovačić <laurakciic@gmail.com>
2021-12-23 23:17:52 +0100

    set basic CRUD for images

--- lv7-MyImgur-site/myimgur/app/urls.py
+++ lv7-MyImgur-site/myimgur/app/urls.py
@@ -3,9 +3,11 @@ from django.urls import path
 from . import views
 
 app_name = 'app'    # sve rute ce bit u app (pocinjat ce sa app:index, app:detail, app:comment)
-urlpatterns = [
+urlpatterns = [     # naziv viewa i naziv rute nije povezan!
     path('', views.index, name='index'),
-    path('<int:image_id>/', views.detail, name='detail'),           # ako imamo images/broj/ onda to renderira detail
+    path('<int:image_id>/', views.detail, name='detail'),           # (CRUD-R), ako imamo images/broj/ onda to renderira detail
     path('<int:image_id>/comment', views.comment, name='comment'),  # ako imamo images/broj/comment onda se to salje na views.comment
-    path('new', views.create_image, name="create_image"),   # putanja: localhost8000/images/new, app:create_image
+    path('new', views.create_image, name="create_image"),           # (CRUD-C), putanja: localhost8000/images/new, app:create_image
+    path('<int:image_id>/edit', views.update_image, name="edit_image"),         # (CRUD-U)
+    path('<int:image_id>/delete', views.delete_image, name="delete_image"),     # (CRUD-D)
 ]
--- lv7-MyImgur-site/myimgur/app/views.py
+++ lv7-MyImgur-site/myimgur/app/views.py
@@ -28,8 +28,27 @@ def create_image(request):              # image_id nije potreban jer kad pravimo
     else:
         form = ImageForm()              # ako nije POST req (ako nije submitana), izgenerirat ce novu praznu formu 
     
-    context = { 'form': form }          # posto nas template uvijek prima context trebamo tu var napravit
+    context = { 'form': form, 'action': 'create' }   # posto nas template uvijek prima context trebamo tu var napravit, action create zbog razlike dvije akcije gumbova (create image/update image)
     return render(request, 'app/create_image.html', context)    # u render ide request, template i context
+
+def update_image(request, image_id):
+    image = get_object_or_404(Image, pk=image_id)
+    if request.method == 'POST':        # ako je forma submitana
+        form = ImageForm(request.POST, instance=image)  # pri kreiranju forme kreira sve fieldove potrebne za formu i buduci da smo je povezali preko instance sa image, znat ce koji je id te slike i nece pokusat napravit novu, nego ce presnimit podatke koji su se promijenili preko onih postojecih
+        if form.is_valid():
+            saved_image = form.save()   
+            return HttpResponseRedirect(reverse('app:detail', args=(saved_image.id,)))  
+    else:
+        form = ImageForm(instance=image)  # ne radi se novi image nego ce presnimit podatke koji su se promijenili preko onih postojecih
+    
+    context = { 'form': form, 'action': 'update'}          
+    return render(request, 'app/create_image.html', context)  # ne moramo mijenjat template koji se renderira jer ce on sadrzavat sva ista polja i on je minimalan
+
+def delete_image(request, image_id):      # problem kako cemo pozvat delete metodu - konvencija je kad god se na serveru nesto mijenja treba bit post req, a njega ne mozemo dobit samo klikom na link, mora doc do submita forme 
+    image = get_object_or_404(Image, pk=image_id)   # rjesenje je preko skrivenih formi 
+    if request.method == 'POST' and request.user.is_authenticated:
+        image.delete()
+    return HttpResponseRedirect(reverse('app:index'))
 
 def comment(request, image_id):         # na samom detail viewu cemo napravit tu formu i onda ce ovaj comment obradivat samo POST req
     if request.method == 'POST' and request.user.is_authenticated:
